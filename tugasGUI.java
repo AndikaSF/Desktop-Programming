cadf466f1ba5295cde3d8e5c077d04ecbfcd22a2
AndikaSF <93238661+AndikaSF@users.noreply.github.com>
2022-03-15 22:32:46 +0700

    Menentukan Bilangan Ganjil-Genap

---
+++ tugasGUI.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasGUI_2;
+
+/**
+ *
+ * @author AndikaSF18
+ */
+public class tugasGUI {
+    public static void main(String[]args){
+        input it = new input();
+        it.setVisible(true);
+        it.pack();
+        it.setLocationRelativeTo(null);
+        it.setDefaultCloseOperation(input.EXIT_ON_CLOSE);
+    }
+}
